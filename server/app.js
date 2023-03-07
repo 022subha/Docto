4007f3af38ab2911d2de8d9f3bd4a8f7e0775149
Subhajit <022subha@gmail.com>
2023-03-08 01:44:56 +0530

    Some Modification

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-
+//* All routes controller imports
+import test from "./routes/route.js";
 const app = express();
 
 //* Middlewares set-up
@@ -8,8 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
-//* All routes controller imports
-import test from "./routes/route.js";
 //* All routes setup
 app.use("/api", test);
 export default app;
