54f93f62ee84a7e954e56aa432a961b5accf8387
Subhajit <022subha@gmail.com>
2023-03-08 13:52:58 +0530

    mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://subhajit:subhajit@docto-db.29wunss.mongodb.net/docto`
-    );
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Database connected Successfully!!");
   } catch (error) {
     console.log(error);
